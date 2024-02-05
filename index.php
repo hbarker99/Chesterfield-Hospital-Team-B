c5e1ba4c9e8a39fabf6686d2d168937b2e49f0e6
Peter Jackson <45214509+PeterJacksonn@users.noreply.github.com>
2024-02-05 17:48:24 +0000

    Initial index setup

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php require("navBar.php")?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Chesterfield Group B</title>
+    
+  </head>
+
+
+<body>
+    <h1>Hello world</h1>
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="myModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    
+
+</body>
+
+</html>
+
+<?php require ("footer.php");?>
+<script>
+    const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+    myModal.show();
+</script>
