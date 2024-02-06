4401ad3d09b4423a342c89ef1e34cec91ec6aef7
Peter Jackson <dubedoutpeter@gmail.com>
2024-02-06 19:07:13 +0000

    Modal stuff
    shows location on startup
    (?location=LOCATION)
    Where are you heading form
    and switch for accessability info

--- index.php
+++ index.php
@@ -14,15 +14,29 @@
     <h1>Hello world</h1>
 
     <!-- Modal -->
-    <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="myModal" aria-hidden="true">
+    <div class="modal modal-xl fade" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="myModal" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Welcome! You are at: 
+                        <?php 
+                        if(isset($_GET['location'])){ 
+                            echo $_GET['location'];}?> 
+                    </h1>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <form>
+                        <div class="mb-3">
+                            <label for="end-location" class="col-form-label">Where are you heading?</label>
+                            <input type="text" class="form-control" id="end-location">
+                        </div>
+                    </form> 
+                    <div class="mb-3 form-switch">
+                        <input type="checkbox" class="form-check-input" role="switch" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check for accessability information</label>
+                    </div>
+
+
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
