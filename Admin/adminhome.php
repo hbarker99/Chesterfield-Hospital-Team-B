5d6d3fb8b90f27b334293cc3866194d159fd067c
MuhammadIlyas2 <150243304+MuhammadIlyas2@users.noreply.github.com>
2024-02-17 18:07:28 +0000

    Just Commit PLz

---
+++ Admin/adminhome.php
@@ -0,0 +1,4 @@
+<html>
+    <h1>Hello,Welcome to Chesterfield Hospital</h1>
+
+</html>
--- Admin/adminlogin.php
+++ Admin/adminlogin.php
@@ -4,6 +4,25 @@
 </head>
 
 <body>
+<h1> Admin Login </h1>
+    <div class="text-block" id="home">
+        <div class="text-block">
+            <?php
+    include("../class/adminconnections.php");
+    if (($_SERVER["REQUEST_METHOD"] == "POST") && (isset($_POST['login']))) {
+        $username = $_POST['Username'];
+        $password = $_POST['Password'];
+
+        $result = admin_login($Username, $Password);
+        if ($result) {
+            header("Location: ../Admin/adminhome.php?uname=".$Username);
+        } else {
+            echo "invalid credintials";
+        }
+
+    }
+    ?>
+
 <form method="post">
                 <div>
                     <label>Username</label>
