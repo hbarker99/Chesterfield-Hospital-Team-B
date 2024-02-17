f9fc44333580d02895358a6b4296891abb0ebfe5
MuhammadIlyas2 <150243304+MuhammadIlyas2@users.noreply.github.com>
2024-02-17 17:17:13 +0000

    Create adminlogin.php

---
+++ Admin/adminlogin.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+</head>
+
+<body>
+<form method="post">
+                <div>
+                    <label>Username</label>
+                    <input required type="username" name="username" id="username" />
+                </div>
+
+                <div>
+                    <label> Password </label>
+                    <input required type="password" name="password" id="password" />
+                    <br />
+                    <a href="./forgot_password.php"> Forgot Password ? </a>
+                </div>
+
+                <div>
+                    <input type="submit" value="Login" name="login" id="login" />
+                </div>
+            </form>
+    
+
+
+</body>
+
+</html>
