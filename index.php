038d9fdae238daad3742a3456b4b65b5d1e35b72
Peter Jackson <45214509+PeterJacksonn@users.noreply.github.com>
2024-02-19 15:11:54 +0000

    form experimenting

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php require("navBar.php");
+<?php
+require("navBar.php");
 include_once("dbString.php");
-
 
 function locationFill(){
 
@@ -23,11 +23,11 @@ function startLocation() {
     $data = $result->fetchArray(SQLITE3_ASSOC);    
     return $data['name'];
 }
-
 require ("footer.php");
 ?>
 
 <!doctype html>
+
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -35,11 +35,10 @@ require ("footer.php");
     <title>Chesterfield Group B</title>
   </head>
 
-
-
 <body id="bootstrap-overrides">
     <h1>Hello world</h1>
 
+    <p id="demo"></p>
     <!-- Modal -->
     <div class="modal modal-xl fade" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="myModal" aria-hidden="true">
         <div class="modal-dialog">
@@ -53,24 +52,24 @@ require ("footer.php");
                     </h1>
                 </div>
                 <div class="modal-body">
-                    <?php if(!isset($_GET['location'])): ?> 
-                    <div class="form-floating">
-                        <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
-                            <option selected>Where are you located?</option>
-                            <?php
-                            $locations = locationFill();
-                            foreach($locations as $location){
-                                echo '<option value="'.$location['name'].'">'.$location['name'].'</option>';
-                            }
-                            ?>
-                        </select>
-                        <label for="floatingSelect">Pick your location</label>
-                    </div> 
-                    <?php endif; ?>
- 
-                    <form>
+                    <form onsubmit="return false">
+                        <?php if(!isset($_GET['location'])): ?> 
                         <div class="form-floating">
-                            <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                            <select class="form-select" id="startLocation" aria-label="startLocation">
+                                <option selected>Where are you located?</option>
+                                <?php
+                                $locations = locationFill();
+                                foreach($locations as $location){
+                                    echo '<option value="'.$location['name'].'">'.$location['name'].'</option>';
+                                }
+                                ?>
+                            </select>
+                            <label for="startLocation">Pick your location</label>
+                        </div> 
+                        <?php endif; ?>
+ 
+                        <div class="form-floating">
+                            <select class="form-select" id="endLocation" aria-label="endLocation">
                                 <option selected>Where are you headed?</option>
                                 <?php
                                 $locations = locationFill();
@@ -79,24 +78,22 @@ require ("footer.php");
                                 }
                                 ?>
                             </select>
-                            <label for="floatingSelect">Pick your goal location</label>
+                            <label for="endLocation">Pick your goal location</label>
                         </div> 
+
                         <div class="mb-4 form-switch">
-                            <input type="checkbox" class="form-check-input" role="switch" id="exampleCheck0">
+                            <input type="checkbox" class="form-check-input" role="switch" id="accessibilitySwitch">
                             <label class="form-check-label" for="exampleCheck0">Check for accessibility information</label>
                         </div>
-                    </form> 
                 </div>
 
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Get route</button>
-
-
+                    <button type="submit" onclick="getRoute()" class="btn btn-primary" data-bs-dismiss="modal">Get route</button>
                 </div>
+
+                </form> 
             </div>
         </div>
-
-
     </body>
 </html>
 
@@ -104,4 +101,10 @@ require ("footer.php");
 <script>
     const myModal = new bootstrap.Modal(document.getElementById('myModal'));
     myModal.show();
+</script>
+<script>
+    function getRoute(){
+    text = "hellooooo"
+    document.getElementById("demo").innerHTML = text;
+    }
 </script>
