f1bb56d661612361c03d3839af56e48679aa8e5f
Henry Barker <henry_barker99@hotmail.com>
2024-02-19 11:17:01 +0000

    Colours and buttons

--- index.php
+++ index.php
@@ -14,51 +14,51 @@ include_once("dbString.php");
   </head>
 
 
-<body>
-    <h1>Hello world</h1>
+    <body>
+        <h1>Hello world</h1>
 
-    <!-- Modal -->
-    <div class="modal modal-xl fade" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="myModal" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Welcome!
-                        <?php if(isset($_GET['location'])): ?>
-                        You are at: 
-                        <?php echo($_GET['location']); endif; ?>
-                    </h1>
-                </div>
-                <div class="modal-body">
-                    <?php if(!isset($_GET['location'])): ?> 
-                    <form>
-                        <div class="mb-3">
-                            <label for="start-location" class="col-form-label">Where are you located?</label>
-                            <input type="text" class="form-control" id="start-location">
-                        </div>
-                    </form>
-                    <?php endif; ?>
- 
-                    <form>
-                        <div class="mb-3">
-                            <label for="end-location" class="col-form-label">Where are you heading?</label>
-                            <input type="text" class="form-control" id="end-location">
-                        </div>
-                        <div class="mb-4 form-switch">
-                            <input type="checkbox" class="form-check-input" role="switch" id="exampleCheck0">
-                            <label class="form-check-label" for="exampleCheck0">Check for accessibility information</label>
-                        </div>
-                    </form> 
-                    
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Get route</button>
+        <!-- Modal -->
+        <div class="modal modal-xl fade" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="myModal" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Welcome!
+                            <?php if(isset($_GET['location'])): ?>
+                            You are at: 
+                            <?php echo($_GET['location']); endif; ?>
+                        </h1>
+                    </div>
+                    <div class="modal-body">
+                        <?php if(!isset($_GET['location'])): ?> 
+                        <form>
+                            <div class="mb-3">
+                                <label for="start-location" class="col-form-label">Where are you located?</label>
+                                <input type="text" class="form-control" id="start-location">
+                            </div>
+                        </form>
+                        <?php endif; ?>
+    
+                        <form>
+                            <div class="mb-3">
+                                <label for="end-location" class="col-form-label">Where are you heading?</label>
+                                <input type="text" class="form-control" id="end-location">
+                            </div>
+                            <div class="mb-4 form-switch">
+                                <input type="checkbox" class="form-check-input" role="switch" id="exampleCheck0">
+                                <label class="form-check-label" for="exampleCheck0">Check for accessibility information</label>
+                            </div>
+                        </form> 
+                        
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="button-primary" data-bs-dismiss="modal">Get route</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 
-</body>
+    </body>
 
 </html>
 
