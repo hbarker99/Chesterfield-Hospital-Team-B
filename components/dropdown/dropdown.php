cf5abb24c8aeb730769fbf01edb821a9a6ddd9d8
Henry Barker <henry_barker99@hotmail.com>
2024-02-20 11:51:57 +0000

    Use Id

--- components/dropdown/dropdown.php
+++ components/dropdown/dropdown.php
@@ -1,7 +1,8 @@
 <div class="dropdown">
-    <input type="text" class="form-control searchInput" placeholder="Search...">
+    <input type="text" class="form-control searchInput" id="<?php echo $DropdownId ?>" placeholder="Search...">
     <input type="hidden" id="dropdownValue">
     <div class="dropdown-content"></div>
 </div>
+
 <script src="components/dropdown/dropdown.js"></script>
 <link rel="stylesheet" href="components/dropdown/dropdown.css"/>
