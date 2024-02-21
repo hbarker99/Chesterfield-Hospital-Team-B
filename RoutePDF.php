a513897edcd0a27e29fda023bb9b8f97160a5493
Peter Jackson <45214509+PeterJacksonn@users.noreply.github.com>
2024-02-21 09:30:54 +0000

    session handling set up

--- RoutePDF.php
+++ RoutePDF.php
@@ -1,26 +1,8 @@
-<?php
-session_start();
+<?php 
+include "sessionHandling.php";
 
-// Check if the session data exists and is not empty
-if(isset($_SESSION['routeInfo']) && !empty($_SESSION['routeInfo'])) {
-    // Retrieve the POST data stored in the session
-    $routeInfo = $_SESSION['routeInfo'];
-    
-    // Now you can access the POST data as needed
-    $startPoint = $routeInfo['startPoint'];
-    $endPoint = $routeInfo['endPoint'];
-    // Check if the 'accessibilityCheck' key exists in the array before accessing it
-    $accessibilityCheck = isset($routeInfo['accessibilityCheck']) ? $routeInfo['accessibilityCheck'] : ''; 
-     
-    // Now you can process the data further or use it as required
-    
-    // Don't forget to unset or clear the session data if it's no longer needed
-    // unset($_SESSION['routeInfo']);
-} else {
-    // Handle the case where session data is not available
-    echo "Session data not found!";
-}
 
 echo "start point " . $startPoint . "\n";
 echo "end point " . $endPoint . "\n";
 echo "accessibility check " . $accessibilityCheck . "\n"; 
+?>
