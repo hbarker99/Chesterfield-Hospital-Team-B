d7f14aeba69621af1cf794359a126d8e3570b9fb
Peter Jackson <45214509+PeterJacksonn@users.noreply.github.com>
2024-02-21 15:43:23 +0000

    barebones but works routePDF

--- RoutePDF.php
+++ RoutePDF.php
@@ -1,6 +1,21 @@
 <?php 
 include("sessionHandling.php");
 include("mapping-algo.php");
+include("dbString.php");
+
+function getStartEndNames($nodeId) {
+    $db = new SQLite3(get_string());
+    $stmt = $db->prepare('SELECT name FROM Node WHERE node_id=:nodeId');
+    $stmt->bindParam(':nodeId', $nodeId, SQLITE3_INTEGER);
+    $result = $stmt->execute();
+    $data = $result->fetchArray(SQLITE3_ASSOC);    
+    return $data['name'];
+}
+
+$startName = getStartEndNames($startPoint);
+$endName = getStartEndNames($endPoint);
+
+
 
 ob_clean();
 include('./PDF/fpdf186/fpdf.php');
@@ -10,13 +25,16 @@ $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->setFont('Arial' ,'B', 24);
 $pdf->Image('./PDF/NHSBlue.jpg', 150, null, 50);
-$pdf->Cell(0,20,'Hello! Your directions:', 0, 1, 'C');
+$pdf->Ln();
+$pdf->Ln();
+$directionInfo = 'Your directions for ' . $startName . ' to ' . $endName . ':';
+$pdf->MultiCell(0,10, $directionInfo);
 
 // Loop through each step and add content to the PDF
 foreach ($final_path as $index => $step) {
     $directions = 'Step ' . ($index + 1). ': ' . $step['notes'] . ' Now turn ' . $step['direction'] . '.';
     $pdf->SetFont('Arial', '', 14);
-    $pdf->MultiCell(0,10,$directions);
+    $pdf->MultiCell(0,5,$directions);
     $pdf->Ln();
     //$pdf->Image('./img/' . $step['image'], 55, null, 100);
 }
