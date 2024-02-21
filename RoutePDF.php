2318d461b50ea01c608aa2da8a3167b69f00efc4
Peter Jackson <45214509+PeterJacksonn@users.noreply.github.com>
2024-02-21 15:28:17 +0000

    added notes to db, also nhs pngs for now

--- RoutePDF.php
+++ RoutePDF.php
@@ -6,14 +6,19 @@ ob_clean();
 include('./PDF/fpdf186/fpdf.php');
 $pdf = new FPDF();
 
+// NHS looking these are you steps page (maybe not needed)
+$pdf->AddPage();
+$pdf->setFont('Arial' ,'B', 24);
+$pdf->Image('./PDF/NHSBlue.jpg', 150, null, 50);
+$pdf->Cell(0,20,'Hello! Your directions:', 0, 1, 'C');
+
 // Loop through each step and add content to the PDF
 foreach ($final_path as $index => $step) {
-    $pdf->AddPage();
-    $pdf->SetFont('Arial', '', 18);
-    $pdf->Cell(0, 10, 'Step ' . ($index + 1), 0, 1, 'C');
-    $pdf->Cell(0, 10, 'Turn ' . $step['direction'], 0, 1, 'C');
+    $directions = 'Step ' . ($index + 1). ': ' . $step['notes'] . ' Now turn ' . $step['direction'] . '.';
+    $pdf->SetFont('Arial', '', 14);
+    $pdf->MultiCell(0,10,$directions);
     $pdf->Ln();
-    $pdf->Image('./img/' . $step['image'], 55, null, 100);
+    //$pdf->Image('./img/' . $step['image'], 55, null, 100);
 }
 
 $pdf->Output();
