cf9e88bfd9a95b58d209fa3fe65db503bc263639
Peter Jackson <45214509+PeterJacksonn@users.noreply.github.com>
2024-02-21 12:50:22 +0000

    pdf viewer

--- RoutePDF.php
+++ RoutePDF.php
@@ -1,7 +1,19 @@
 <?php 
 include("sessionHandling.php");
-echo "start point " . $startPoint . "\n";
-echo "end point " . $endPoint . "\n";
-echo "accessibility check " . $accessibilityCheck . "\n"; 
 include("mapping-algo.php");
-?>
+
+ob_end_clean();
+include('./PDF/fpdf186/fpdf.php');
+$pdf = new FPDF();
+
+// Loop through each step and add content to the PDF
+foreach ($final_path as $index => $step) {
+    $pdf->AddPage();
+    $pdf->SetFont('Arial', '', 18);
+    $pdf->Cell(0, 10, 'Step ' . ($index + 1), 0, 1, 'C');
+    $pdf->Cell(0, 10, 'Turn ' . $step['direction'], 0, 1, 'C');
+    $pdf->Ln();
+    $pdf->Image('./img/' . $step['image'], 50, null, 100);
+}
+
+$pdf->Output();
