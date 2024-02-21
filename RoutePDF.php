7560ddff113a6c40715d1d7db92bf9e9f15a328d
Peter Jackson <45214509+PeterJacksonn@users.noreply.github.com>
2024-02-21 10:37:20 +0000

    pdf library and session handling

--- RoutePDF.php
+++ RoutePDF.php
@@ -1,7 +1,6 @@
 <?php 
-include "sessionHandling.php";
-
-
+include_once("sessionHandling.php");
+require("./PDF/fdpf/fpdf.php");
 echo "start point " . $startPoint . "\n";
 echo "end point " . $endPoint . "\n";
 echo "accessibility check " . $accessibilityCheck . "\n"; 
