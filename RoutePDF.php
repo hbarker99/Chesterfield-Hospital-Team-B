2a35250e18d7701769a5cc4c71c83cc41657b01f
Peter Jackson <45214509+PeterJacksonn@users.noreply.github.com>
2024-02-22 11:57:34 +0000

    route pdf

--- RoutePDF.php
+++ RoutePDF.php
@@ -1,8 +1,10 @@
 <?php 
+include('./PDF/fpdf186/fpdf.php');
 include("sessionHandling.php");
 include("mapping-algo.php");
 include("dbString.php");
 
+// Function to get the names of the start node and end node
 function getStartEndNames($nodeId) {
     $db = new SQLite3(get_string());
     $stmt = $db->prepare('SELECT name FROM Node WHERE node_id=:nodeId');
@@ -11,32 +13,38 @@ function getStartEndNames($nodeId) {
     $data = $result->fetchArray(SQLITE3_ASSOC);    
     return $data['name'];
 }
-
 $startName = getStartEndNames($startPoint);
 $endName = getStartEndNames($endPoint);
 
-
-
+// Creating PDF instance
 ob_clean();
-include('./PDF/fpdf186/fpdf.php');
 $pdf = new FPDF();
-
-// NHS looking these are you steps page (maybe not needed)
 $pdf->AddPage();
-$pdf->setFont('Arial' ,'B', 24);
-$pdf->Image('./PDF/NHSBlue.jpg', 150, null, 50);
-$pdf->Ln();
-$pdf->Ln();
+$pdf->setFont('Arial' ,'', 22);
+
+// NHS logo and Directions header
+//$pdf->Image('./PDF/NHSBlue.jpg', 155, null, 50);
+$pdf->Image('./PDF/CRHNHS.png', 155, null, 50);
+//$pdf->Ln(10);
 $directionInfo = 'Your directions for ' . $startName . ' to ' . $endName . ':';
-$pdf->MultiCell(0,10, $directionInfo);
+$pdf->MultiCell(0,10, $directionInfo, 0, 'C');
+$pdf->Ln();
 
 // Loop through each step and add content to the PDF
 foreach ($final_path as $index => $step) {
-    $directions = 'Step ' . ($index + 1). ': ' . $step['notes'] . ' Now turn ' . $step['direction'] . '.';
+    if ($step['direction'] == 'left' || $step['direction'] == 'right'){
+        $movementDescriptor = ' Turn';
+    } else if ($step['direction'] == 'upstairs' || $step['direction'] == 'downstairs') {
+        $movementDescriptor = ' Use the stairs/lift to continue';
+    } 
+
+    // Step #: *Edge notes* . Direction change . Direction
+    // Step 1: You will see... Turn right. 
+    $pdf->SetFont('Arial', 'B', 14);
+    $pdf->Cell(0, 10, 'Step ' . ($index + 1), 0, 1,);
     $pdf->SetFont('Arial', '', 14);
+    $directions = $step['notes'] . $movementDescriptor . ' ' .  $step['direction'] . '.';
     $pdf->MultiCell(0,5,$directions);
     $pdf->Ln();
-    //$pdf->Image('./img/' . $step['image'], 55, null, 100);
 }
-
 $pdf->Output();
