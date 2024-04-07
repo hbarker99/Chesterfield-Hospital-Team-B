95e2295835d487968cf58a59a6d0ef19f6724e1e
hbarker99 <henry_barker99@hotmail.com>
2024-04-07 13:40:16 +0100

    Add login

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/user.php');
 $requestUrl = isset($_GET['url']) ? $_GET['url'] : '';
+
 
 $routes = [
     '' => '/pages/route-picker/route-picker.php',
     'route' => '/pages/route-picker/route-picker.php',
     'show-route' => '/pages/route-picker/show/route.php',
     'map' => '/pages/map/map.php',
+    'admin' => '/pages/login/login.php',
     'show-pdf' => '/pages/route-picker/pdf/route-pdf.php'
 ];
 
+$requiresAuth = [
+    'map' => TRUE,
+];
+
 if (array_key_exists($requestUrl, $routes)) {
+    if (array_key_exists($requestUrl, $requiresAuth) && $_SESSION['user'] == NULL) {
+        header("Location: /admin");
+        exit;
+    }
+
     include $_SERVER['DOCUMENT_ROOT'] . $routes[$requestUrl];
 } else {
     http_response_code(404);
-    include($_SERVER['DOCUMENT_ROOT'] . "/error.php");
+    include($_SERVER['DOCUMENT_ROOT'] . '/error.php');
 }
 ?>
--- pages/login/login.php
+++ pages/login/login.php
@@ -1,6 +1,6 @@
 <?php
-
-$db = new SQLite3("admin_database.db");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/user.php");
+$db = new SQLite3($_SERVER['DOCUMENT_ROOT'] . "/pages/login/admin_database.db");
 $stmt = $db->prepare('SELECT username, password FROM Login WHERE username=:username AND password=:password');
 
 $stmt->bindParam(':username', $_POST['username'], SQLITE3_TEXT);
@@ -16,6 +16,7 @@ while ($row = $result->fetchArray()) {
 
 if (isset($_POST['login'])) {
     if ($users != null) {
+        $_SESSION['user'] = $users[0]['username'];
         header("Location: /map");
     } else {
         echo '<script>alert("Wrong Username or Password")</script>';
@@ -50,7 +51,6 @@ if (isset($_POST['login'])) {
 
                 <div class="form-actions">
                     <input type="submit" value="Login" name="login" id="login" />
-                    <a href="/pages/login/forgot/forgot_password.php">Forgot Password?</a>
                 </div>
             </form>
         </div>
